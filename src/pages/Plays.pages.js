b45e1aa9eb64c93bb693880959a25c0c7191a3bc
Shailee Shah <83085462+shailee2036@users.noreply.github.com>
2021-09-12 18:25:51 +0530

    Update Plays.pages.js

--- src/pages/Plays.pages.js
+++ src/pages/Plays.pages.js
@@ -1,51 +1,37 @@
-import React from "react";
-import Slider from "react-slick";
-
-
-const PlaysCard = (props) => {
+import React from 'react';
+import PlaysRow from '../components/Poster/PlaysPoster.component';
+import playsImages from '../config/PlaysPoster.config';
+const Plays = () => {
   return (
     <>
-    <div className="w-full h-30 px-2">
-    <img
-      className="w-full h-full rounded-xl"
-      src={props.src}
-      alt="Entertainment Image"
-     />
-    </div>
+      <div className='flex flex-row'>
+        <div className='text-center ml-10 '>
+          <h1 className='text-3xl text-gray-700 font-bold my-12'>Filters</h1>
+        </div>
+        <div className='my-12 lg:ml-80 '>
+          <h1 className='text-3xl text-gray-700 font-bold'>
+            Plays in Bhavnagar
+          </h1>
+          <button className='btn border-2 my-5 py-1 px-3 text-sm border-gray-300 bg-white rounded-2xl text-vaish-300'>
+            Theatre
+          </button>
+          <div className='flex flex-row py-5'>
+            <PlaysRow images={playsImages} />
+          </div>
+          <div className='flex flex-row py-5'>
+            <PlaysRow images={playsImages} />
+          </div>
+          <div className='flex flex-row py-5'>
+            <PlaysRow images={playsImages} />
+          </div>
+          <div className='flex flex-row py-5'>
+            <PlaysRow images={playsImages} />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
-const PlaysCourousal = () => {
+export default Plays;
 
-  const PlaysImage = [
-
-   "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxMiBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314523-lychsakbav-portrait.jpg",
-   "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOSBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314534-ktzzmvqrht-portrait.jpg",
-   "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxMiBTZXAgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:oi-discovery-catalog@@icons@@bundle-icon-shadow-4x.png,ox-15,oy-15,ow-50/et00304172-pclggvbndr-portrait.jpg",
-   "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxMiBTZXAgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:oi-discovery-catalog@@icons@@bundle-icon-shadow-4x.png,ox-15,oy-15,ow-50/et00304172-pclggvbndr-portrait.jpg",
-
-];
-
-
-const settings = {
-  infinite: true,
-  autoplay: false,
-  slidesToShow: 4,
-  slidesToScroll: 0,
-  InitialSlide: 0,
-
-}
-
-return (
-  <>
-  <Slider {...settings}>
-  {PlaysImage.map((image) => (
-    <PlaysCard src={image} />
-  ))}
-  </Slider>
-  </>
-)
-};
-
-export default PlaysCourousal;
