a071a6f59d1828597ccecf0673cf6dd0b95cad7f
shailee2036 <shailee20edu@gmail.com>
2022-01-12 11:50:05 +0530

    final update

--- src/components/MovieHero/Cast.component.js
+++ src/components/MovieHero/Cast.component.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Cast = (props) => {
   return (
-    <div className='flex flex-col justify-center'>
+    <div className='flex flex-col items-center'>
+    <div className='w-32 h-32'>
       <img
-        className=' px-3 rounded-full h-20 w-25'
-        src={props.src}
+        className=' rounded-full h-full w-full'
+        src={props.image}
         alt='cast'
       />
+      </div>
       <h4 className='mx-auto mt-2'>{props.name}</h4>
       <h4 className=' mx-auto text-gray-500'>{props.castName}</h4>
     </div>
--- src/layouts/Movie.layout.js
+++ src/layouts/Movie.layout.js
@@ -1,10 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+
 
 //components
 import MovieNavbar from "../components/Navbar/movieNavbar.component";
 
+// context
+import { MovieContext } from "../context/movie.context";
+
+
 
 const MovieLayout = (props) => {
+
+  const { id } = useParams();
+ const { movie, setMovie } = useContext(MovieContext);
+
+ useEffect(() => {
+   const requestMovie = async () => {
+     const getMovieData = await axios.get(`/movie/${id}`);
+     setMovie(getMovieData.data);
+   };
+   requestMovie();
+ }, [id]);
   return (
     <>
     <MovieNavbar />
